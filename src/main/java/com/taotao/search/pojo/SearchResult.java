8ccd9cf0fc3725194b95e5d8632d19777f2cd60e
xdzyq0913 <zyq565478782@qq.com>
2017-03-16 17:26:40 +0800

    first commit

---
+++ src/main/java/com/taotao/search/pojo/SearchResult.java
@@ -0,0 +1,42 @@
+package com.taotao.search.pojo;
+
+import java.util.List;
+
+public class SearchResult {
+	
+	//商品列表
+	private List<Item> itemlist;
+	//总记录数
+	private long recordCount;
+	//总页数
+	private long pageCount;
+	//当前页
+	private long curPage;
+	public List<Item> getItemlist() {
+		return itemlist;
+	}
+	public void setItemlist(List<Item> itemlist) {
+		this.itemlist = itemlist;
+	}
+	public long getRecordCount() {
+		return recordCount;
+	}
+	public void setRecordCount(long recordCount) {
+		this.recordCount = recordCount;
+	}
+	public long getPageCount() {
+		return pageCount;
+	}
+	public void setPageCount(long pageCount) {
+		this.pageCount = pageCount;
+	}
+	public long getCurPage() {
+		return curPage;
+	}
+	public void setCurPage(long curPage) {
+		this.curPage = curPage;
+	}
+
+
+
+}
